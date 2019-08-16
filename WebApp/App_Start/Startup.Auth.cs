8467e3e2ddbe079f7ba9f5769147fc0823ec2f01
Akshat Goel <47198486+akgoel23@users.noreply.github.com>
2019-08-16 15:38:57 -0700

    Commit

---
+++ WebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApp.Utils;
+using System;
+
+namespace WebApp
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            // Custom middleware initialization. This is activated when the code obtained from a code_grant is present in the querystring (&code=<code>).
+            app.UseOAuth2CodeRedeemer(
+                new OAuth2CodeRedeemerOptions
+                {
+                    ClientId = AuthenticationConfig.ClientId,
+                    ClientSecret = AuthenticationConfig.ClientSecret,
+                    RedirectUri = AuthenticationConfig.RedirectUri
+                }
+                );
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    // The `Authority` represents the AD FS - https://[your AD FS hostname]/adfs
+                    Authority = AuthenticationConfig.Authority,
+                    ClientId = AuthenticationConfig.ClientId,
+                    RedirectUri = AuthenticationConfig.RedirectUri,
+                    PostLogoutRedirectUri = AuthenticationConfig.RedirectUri,
+                    //Scope = String.Format("{0}/openid", AuthenticationConfig.Resource)  + String.Format(" {0}/Mail.Read", AuthenticationConfig.Resource), // a basic set of permissions for user sign in & profile access "openid profile offline_access"
+                    Scope = String.Format("{0}/openid", AuthenticationConfig.Resource), // a basic set of permissions for user sign in & profile access "openid profile offline_access"
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        // In a real application you would use IssuerValidator for additional checks, like making sure the user's organization has signed up for your app.
+                        //     IssuerValidator = (issuer, token, tvp) =>
+                        //     {
+                        //        //if(MyCustomTenantValidation(issuer))
+                        //        return issuer;
+                        //        //else
+                        //        //    throw new SecurityTokenInvalidIssuerException("Invalid issuer");
+                        //    },
+                        //NameClaimType = "name",
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        AuthorizationCodeReceived = OnAuthorizationCodeReceived,
+                        AuthenticationFailed = OnAuthenticationFailed,
+                    }
+                });
+        }
+
+        private async Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedNotification context)
+        {
+            // Upon successful sign in, get the access token & cache it using MSAL
+            IConfidentialClientApplication clientApp = MsalAppBuilder.BuildConfidentialClientApplication(new ClaimsPrincipal(context.AuthenticationTicket.Identity));
+            AuthenticationResult result = await clientApp.AcquireTokenByAuthorizationCode(new[] { String.Format(" {0}/openid", AuthenticationConfig.Resource) }, context.Code).ExecuteAsync();
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+            notification.Response.Redirect("/Error?message=" + notification.Exception.Message);
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Utils;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Name = ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+            ViewBag.AuthorizationRequest = string.Empty;
+
+            // The object ID claim will only be emitted for work or school accounts at this time.
+            Claim oid = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier");
+            ViewBag.ObjectId = oid == null ? string.Empty : oid.Value;
+
+            // The 'preferred_username' claim can be used for showing the user's primary way of identifying themselves
+            ViewBag.Username = ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+
+            // The subject or nameidentifier claim can be used to uniquely identify the user
+            ViewBag.Subject = ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> AccessToken()
+        {
+            // Before we render the send email screen, we use the incremental consent to obtain and cache the access token with the correct scopes
+            IConfidentialClientApplication app = MsalAppBuilder.BuildConfidentialClientApplication();
+            AuthenticationResult result = null;
+            var accounts = await app.GetAccountsAsync();
+            string[] scopes = { String.Format(" {0}/openid", AuthenticationConfig.Resource) };
+
+            try
+            {
+                // try to get an already cached token
+                result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync().ConfigureAwait(false);
+                ViewBag.AccessToken = result.AccessToken;
+                ViewBag.Environment = result.Account.Environment;
+
+                // The 'preferred_username' claim can be used for showing the user's primary way of identifying themselves
+                ViewBag.Username = result.Account.Username;
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync.
+                // This indicates you need to call AcquireTokenAsync to acquire a token
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    // Build the auth code request Uri
+                    string authReqUrl = await OAuth2RequestManager.GenerateAuthorizationRequestUrl(scopes, app, this.HttpContext, Url);
+                    ViewBag.AuthorizationRequest = authReqUrl;
+                    ViewBag.Relogin = "true";
+                }
+                catch (MsalException msalex)
+                {
+                    Response.Write($"Error Acquiring Token:{System.Environment.NewLine}{msalex}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
+            }
+
+            return View();
+        }
+
+    }
+}
